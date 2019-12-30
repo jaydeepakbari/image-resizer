b43927122e29357b4cc034f7bd6be1d58d5ca154
Jaydeep Akbari <jaydeepakbari@gmail.com>
2019-12-30 15:18:57 +0530

    Upload

---
+++ jd/imageresizer/src/RImageController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace JD\Imageresizer\Controllers;
+use Illuminate\Http\Request;
+
+class RImageController extends Controller{
+	
+}
---
+++ jd/imageresizer/src/RImageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JD\Imageresizer;
+
+use Illuminate\Support\ServiceProvider;
+
+class RImageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
